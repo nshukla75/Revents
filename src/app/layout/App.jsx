ecc1579ca4f168ff158f1a28fa4228b0e2da77a6
Nitu Shukla <nshukla@nshukla-mbp.local>
2018-06-14 10:04:25 -0700

    EventDashboard Initial

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Re-vents</h1>
+        <EventDashboard/>
       </div>
     );
   }
