57a5c4380619bd5f2c398410188e5672b45f7218
Nitu Shukla <nshukla@nshukla-mbp.local>
2018-06-26 17:14:30 -0700

    End of Section 7

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -4,11 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
 import EventForm from '../../features/event/EventForm/EventForm';
-import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+// import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
 import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
 import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import HomePage from '../../features/home/HomePage'
+import TestComponent from '../../features/testarea/TestComponent';
 
 
 class App extends Component {
@@ -26,7 +27,7 @@ class App extends Component {
             <Container className="main">
               <Switch>
                 <Route path='/events' component={EventDashboard}/>
-                <Route path='/event/:id' component={EventDetailedPage}/>
+                <Route path='/test' component={TestComponent}/>
                 <Route path='/people' component={PeopleDashboard}/>
                 <Route path='/profile/:id' component={UserDetailedPage}/>
                 <Route path='/settings' component={SettingsDashboard}/>
